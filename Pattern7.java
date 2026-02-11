3be2b0dba579b672b4a44c32191045e31ede9977
tulasipraneeth <mukkatulasipraneeth@gmail.com>
2026-02-11 16:38:31 +0530

    Update Pattern7.java

--- Pattern7.java
+++ Pattern7.java
@@ -1,1 +1,18 @@
+//Floyd's Triangle Pattern
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 1;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print(cnt++ + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
 
