5bad681b3a4a13ad1ce49f7f04710a4b3fbd54a8
tulasipraneeth <mukkatulasipraneeth@gmail.com>
2026-02-11 16:34:50 +0530

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,19 @@
+//Inverted and Half Rotated Pattern
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for(int j = n - i - 1; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
