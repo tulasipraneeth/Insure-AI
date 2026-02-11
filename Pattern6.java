6402c5bc1ca2ceb7772ba3b224253af614b83221
tulasipraneeth <mukkatulasipraneeth@gmail.com>
2026-02-11 16:36:23 +0530

    Update Pattern6.java

--- Pattern6.java
+++ Pattern6.java
@@ -1,1 +1,16 @@
+//Inverted-HalfPyramid with numbers Pattern:
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i; j++) {
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
 
